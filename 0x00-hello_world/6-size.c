3aeb121c90114266b62bf91f028896cfde7bebef
Inimfon Okoh <inimfonokoh1@gmail.com>
2023-07-13 20:02:18 -0700

    size checked

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - is the main function
+ *
+ * Return: 0
+ */
+
+int main(void)
+	printf("Size of Char Data Types in C = %2d bytes \n", sizeof(char));
+	printf("size of int Date Types in c = %2d bytes \n", sizeof(short int));
+	printf("size of Long int Data Types in c = %2d bytes \n", sizeof(long int));
+	printf("size of Long Long int Data Type in c = %2d byes \n", sizeof(long long int));
+	printf("size of Float Data Types in c = %2d bytes \n", sizeof(float));
+	return (0);
+}
