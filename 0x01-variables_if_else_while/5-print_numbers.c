5b8d79ac28f766e18334fddfa0f72a124ae4c337
Inimfon Okoh <inimfonokoh1@gmail.com>
2023-07-14 23:09:52 -0700

    base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description - 'prints all single digit numbers of base'
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
