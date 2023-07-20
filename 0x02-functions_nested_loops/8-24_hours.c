dc7c82a9f6b0e74bcbcf3ab73c6304a7916d2050
Inimfon Okoh <inimfonokoh1@gmail.com>
2023-07-19 22:04:18 -0700

    hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,18 +12,19 @@ void jack_bauer(void)
 	{
 		for (b = 0; b <= 9; b++)
 		{
-			for (c = 0; c <= 5; c++)
+			if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
 			{
-				for (d = 0; d <= 9; d++)
+				for (c = 0; c <= 5; c++)
 				{
-					if (a >= 2 && b >= 4)
-						break;
-					_putchar(a + 48);
-					_putchar(b + 48);
-					_putchar(58);
-					_putchar(c + 48);
-					_putchar(c + 48);
-					_putchar('\n');
+					for (d = 0; d <= 9; d++)
+					{
+						_putchar(a + 48);
+						_putchar(b + 48);
+						_putchar(58);
+						_putchar(c + 48);
+						_putchar(d + 48);
+						_putchar('\n');
+					}
 				}
 			}
 		}
