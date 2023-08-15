84d6780da664a3051e71985e0398b48c588e4820
Inimfon Okoh <inimfonokoh1@gmail.com>
2023-08-15 15:15:50 -0700

    dog_h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -4,7 +4,7 @@
 /**
  * struct dog - A new type describing a dog
  * @name: The name of the dog.
- * @agee: The age of the dog.
+ * @age: The age of the dog.
  * @owner: The owner of the dog.
  */
 struct dog
